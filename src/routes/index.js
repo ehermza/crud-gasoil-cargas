5ae00ee06c8f9b3bb76991dd523a334be6a87d1b
EHER.MZA <ehermza@yahoo.com>
2021-07-17 22:56:22 -0300

    Testing the week-number function...

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,20 @@
 const { Router } = require("express");
 const Carga = require("../models/Carga");
+const { weekNumber } = require('weeknumber');
 
 const router = Router();
 
-router.get('/', async(req, res) => {
+router.get('/week', function () {
+    // const hoy = new Date(2016, 0, 3, 12);//
+    const hoy = new Date();
+    const week = weekNumber(hoy);
+    console.log(`today is: ${hoy}`);
+    console.log('Week of year: ' + week); // Sun
+});
+
+router.get('/', async (req, res) => {
     const cargas = await Carga.find();
-    res.render('index', { cargas:cargas });
+    res.render('index', { cargas: cargas });
 });
 
 router.post('/add', async (req, res) => {
@@ -17,15 +26,15 @@ router.post('/add', async (req, res) => {
     res.redirect('/');
 });
 
-router.get('/delete/:getid', async(req, res,next) => {
+router.get('/delete/:getid', async (req, res, next) => {
     const { getid } = req.params;
-     const carga = await Carga.findByIdAndDelete(getid);
+    const carga = await Carga.findByIdAndDelete(getid);
     console.log(`Delete object: /${getid}`);
 
     res.redirect('/');
 });
 
-router.get('/:getid', async(req, res,next) => {
+router.get('/:getid', async (req, res, next) => {
     const { getid } = req.params;
     const carga = await Carga.findOne(getid);
     console.log(`Get object: /${getid}`);
